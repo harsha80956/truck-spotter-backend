b8cf97bf122a789ce96f87b573aa3fcca048126e
developer <harsha@Booked-MacBook-Pro.local>
2025-03-19 01:56:21 +0530

    host fix

--- spotter_backend/settings.py
+++ spotter_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-change-this-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', 'yes', '1')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
